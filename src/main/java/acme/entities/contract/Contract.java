84e2d308b63ba807aaa4c19dab18b028a0f51170
Maria del Carmen Garcia Garcia <margargar81@alum.us.es>
2024-10-13 17:34:26 +0200

    S2 Entities and Sample Data

---
+++ src/main/java/acme/entities/contract/Contract.java
@@ -0,0 +1,76 @@
+
+package acme.entities.contract;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
+import acme.entities.project.Project;
+import acme.roles.Client;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Contract extends AbstractEntity {
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "^[A-Z]{1,3}-\\d{3}$", message = "[A-Z]{1,3}-[0-9]{3}")
+	protected String			code;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	protected Date				instantiationMoment;
+
+	@NotBlank
+	@Length(min = 1, max = 75)
+	protected String			providerName;
+
+	@NotBlank
+	@Length(min = 1, max = 75)
+	protected String			customerName;
+
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String			goals;
+
+	@Valid
+	@NotNull
+	protected Money				budget;
+
+	protected boolean			draftMode;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Client			client;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Project			project;
+}
